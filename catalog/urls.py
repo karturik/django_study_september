19b9b65a9f3065c8c7a0129145f8034623022298
karturik <kazukevich.artur@gmail.com>
2024-09-17 20:49:15 +0300

    add catalog main page, add simple home page, add base generic, add navbar

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
-import views
+from django.urls import path, re_path
+from . import views
 
 
 urlpatterns = [
         path('', views.catalog_main, name='catalog_main'),
+        # re_path(r'^$', views.index, name='index'),
+        re_path(r'^books/$', views.BookListView.as_view(), name='books'),
     ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from .models import Book, Author, BookInstance, Genre
 
 # Create your views here.
+def catalog_main(request):
+    """
+        Функция отображения для домашней страницы сайта.
+    """
+    # Генерация "количеств" некоторых главных объектов
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    # Доступные книги (статус = 'a')
+    # SELECT * FROM public.library
+    # SELECT count(*) FROM public.library
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_authors=Author.objects.count()  # Метод 'all()' применён по умолчанию.
+
+    context = {'num_books':num_books,
+               'num_instances':num_instances,
+               'num_instances_available':num_instances_available,
+               'num_authors':num_authors}
+    
+    # Отрисовка HTML-шаблона catalog_main_page.html с данными внутри
+    # переменной контекста context
+    return render(
+        request=request,
+        template_name='catalog_main_page.html',
+        context=context
+    )
