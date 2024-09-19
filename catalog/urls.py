a2cb1c9dfcc1d012b21615360a98ac0f8ec5d4f8
karturik <kazukevich.artur@gmail.com>
2024-09-19 21:02:01 +0300

    add class-based views, add pagination, html template

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
         path('', views.catalog_main, name='catalog_main'),
         # re_path(r'^$', views.index, name='index'),
-        re_path(r'^books/$', views.BookListView.as_view(), name='books'),
+        re_path(r'^books/$', views.BookListView.as_view(), name='books_list'),
+        re_path(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail')
     ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,8 @@
+from django.views import generic
 from django.shortcuts import render
+
 from .models import Book, Author, BookInstance, Genre
+
 
 # Create your views here.
 def catalog_main(request):
@@ -27,3 +30,41 @@ def catalog_main(request):
         template_name='catalog_main_page.html',
         context=context
     )
+
+
+class BookListView(generic.ListView):
+    model = Book
+    context_object_name = 'book_list'   # ваше собственное имя переменной контекста в шаблоне
+    # queryset = Book.objects.filter(title__icontains='war')[:5] # Получение 5 книг, содержащих слово 'war' в заголовке
+    template_name = 'books/books_list_page.html'  # Определение имени вашего шаблона и его расположения
+
+    paginate_by = 2
+    
+    # def get_queryset(self):
+    #     return Book.objects.filter(title__icontains='war')[:5] # Получить 5 книг, содержащих 'war' в заголовке
+
+    def get_context_data(self, **kwargs):
+        # В первую очередь получаем базовую реализацию контекста
+        context = super(BookListView, self).get_context_data(**kwargs)
+        # Добавляем новую переменную к контексту и инициализируем её некоторым значением
+        context['some_data'] = 'This is just some data'
+        return context
+    
+
+# def book_detail_view(request, pk):
+#     try:
+#         book_id = Book.objects.get(pk=pk)
+#     except Book.DoesNotExist:
+#         raise Http404("Book does not exists")
+
+#     #book_id=get_object_or_404(Book, pk=pk)
+
+#     return render(
+#         request,
+#         'catalog/book_detail.html',
+#         context={'book':book_id,}
+#     )
+
+class BookDetailView(generic.DetailView):
+    model = Book
+    template_name = 'books/books_detail_page.html'
